0ee618f18ff23e47640b1f63fc444facbdb730df
Michał Katański <michal.katanski@appvelocity.io>
2023-11-10 00:30:42 +0100

    Imported github module as namespace in AssetsService.ts

--- dist/index.js
+++ dist/index.js
@@ -29898,12 +29898,32 @@ function wrappy (fn, cb) {
 
 "use strict";
 
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.AssetsService = void 0;
-const github_1 = __importDefault(__nccwpck_require__(5438));
+const github = __importStar(__nccwpck_require__(5438));
 const HEADERS_BASE = {
     'X-GitHub-Api-Version': '2022-11-28'
 };
@@ -29915,7 +29935,7 @@ class AssetsService {
     constructor(token, releaseTag) {
         this.token = token;
         this.releaseTag = releaseTag;
-        this.octokit = github_1.default.getOctokit(this.token);
+        this.octokit = github.getOctokit(this.token);
         if (!process.env.GITHUB_REPOSITORY) {
             throw new Error('GITHUB_REPOSITORY not set');
         }
--- src/AssetsService.ts
+++ src/AssetsService.ts
@@ -1,4 +1,4 @@
-import github from '@actions/github'
+import * as github from '@actions/github'
 import fs from 'fs'
 
 type RepoData = {
